093e76960e0b8e0027730606d6bd16e9c41ea3cc
anandxkumar <anandkumar26sep00@gmail.com>
2021-06-29 21:04:41 +0530

    Added Legacy wstep and bmw benchmarks

---
+++ Legacy/Legacy wstep/run.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 21:39:26 2021
+
+@author: erwan
+"""
+
+from os.path import dirname, join
+from xexplorer import parse_files, plot
+
+def test_plot_experiment_results_sample():
+    """Sample code to explore experiment data"""
+
+    # %% DATA
+    # files (and special reading commands)
+    files = {join(dirname(__file__),"Spectrum_Legacy_wstep_2.csv"):{},
+              }
+    df = parse_files(files)
+
+    print(files)
+    #%% PLOT
+
+    plot(df, xval='wstep', yval='calculation_time', title="Legacy Voigt Wstep(CO, NO)")
+
+
+    return
+
+
+if __name__ == '__main__':
+    test_plot_experiment_results_sample()

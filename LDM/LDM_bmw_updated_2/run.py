862be7e6bebd85b94c59c7ce05dc5d020d80f30f
anandxkumar <anandkumar26sep00@gmail.com>
2021-07-22 02:27:40 +0530

    Added LDM Benchmark BMW vs CT

---
+++ LDM/LDM_bmw_updated_2/run.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 21:39:26 2021
+
+@author: erwan
+"""
+
+from os.path import dirname, join
+from xexplorer import parse_files, plot
+
+def test_plot_experiment_results_sample():
+    """Sample code to explore experiment data"""
+
+    # %% DATA
+    # files (and special reading commands)
+    files = {join(dirname(__file__),"LDM_bmw_updated_2.csv"):{},
+              }
+    df = parse_files(files)
+
+    print(files)
+    #%% PLOT
+
+    plot(df, xval='broadening_max_width', yval='calculation_time', title="LDM_bmw_updated_2",
+        color_mapper='broadening_method', size='broadening_max_width', color_mapper_scale=False)
+
+
+    return
+
+
+if __name__ == '__main__':
+    test_plot_experiment_results_sample()

001e2fed6ce5e2866ee56e69ca778293f1ac202a
anandxkumar <anandkumar26sep00@gmail.com>
2021-08-08 23:39:34 +0530

    Add Complexity FFT vs Calculation Time

---
+++ LDM UPDATED/Complexity_FFT/run.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 21:39:26 2021
+
+@author: erwan
+"""
+
+from os.path import dirname, join
+from xexplorer import parse_files, plot
+
+def test_plot_experiment_results_sample():
+    """Sample code to explore experiment data"""
+
+    # %% DATA
+    # files (and special reading commands)
+    files = {join(dirname(__file__),"_fft.csv"):{},
+              }
+    df = parse_files(files)
+
+    print(files)
+    #%% PLOT
+
+    plot(df, xval='complexity_final_fft', yval='calculation_time', title="4.675e-08*(1 + wL*wG)*Spectral_Points*log(Spectral_Points) vs Calculation Time",
+         size='broadening_max_width')
+
+
+    return
+
+
+if __name__ == '__main__':
+    test_plot_experiment_results_sample()
---
+++ LDM/Complexity_FFT_Final/Benchmark.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from statsmodels.api import OLS
+from sklearn.linear_model import LinearRegression
+
+# Reading data from the combined data of fft and FFT
+directory = "/home/pipebomb/Desktop/Visualizations/LDM UPDATED/Complexity_FFT/"
+df = pd.read_csv(directory + "Combined_Final.csv")
+
+# Getting all fft broadening readings
+df_fft = df.loc[df['broadening_method'] == 'fft']
+
+# Computing Complexity of LDM>fft 
+df_fft['Complexity_fft'] = (df['wL']*df['wG'] + 1) * df['Spectral Points']*np.log( df['Spectral Points'])
+
+# Getting x and y
+x_fft = df_fft[['Complexity_fft']]  # for mulitple columns -> df_fft[['Complexity_fft','lines_calculated']]
+y_fft = df_fft['calculation_time']
+
+# Using OLS to find coerrelation and coefficient of multiple linear regression
+model = OLS(y_fft, x_fft)
+result = model.fit()
+print(result.summary())
+
+# or using LinearRegression()
+
+regressor = LinearRegression()
+regressor.fit(x_fft, y_fft)
+
+# display coefficients
+print(regressor.coef_)
+
+# Getting coefficient of multiple linear regression
+coeff = regressor.coef_
+
+# Updating Final complexity
+complexity_final_fft = coeff[0]*df_fft['Complexity_fft']  # + coeff[1]*df_fft['lines_calculated'] (if more columns)
+df_fft['complexity_final_fft'] = complexity_final_fft
+
+# Scatter Plot
+df_fft.plot.scatter('complexity_final_fft', 'calculation_time')
+
+# Saving Updated csv file
+df_fft.to_csv(directory + "_fft.csv", index=False)

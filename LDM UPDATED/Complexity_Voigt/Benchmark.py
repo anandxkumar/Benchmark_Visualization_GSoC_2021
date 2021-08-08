f488837a447e860400fbdfc2c8d3c7bd1594d1a0
anandxkumar <anandkumar26sep00@gmail.com>
2021-08-09 02:05:10 +0530

    Add Complexity Voigt

---
+++ LDM UPDATED/Complexity_Voigt/Benchmark.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from statsmodels.api import OLS
+from sklearn.linear_model import LinearRegression
+
+# Reading data from the combined data of voigt and voigt
+directory = "/home/pipebomb/Desktop/Visualizations/LDM UPDATED/Complexity_Voigt/"
+df = pd.read_csv(directory + "Combined_Final.csv")
+
+# Getting all voigt broadening readings
+df_voigt = df.loc[df['broadening_method'] == 'voigt']
+
+# Computing Complexity of LDM>voigt 
+df_voigt['Complexity_voigt'] = (df['wL']*df['wG'] + 1) * df['Spectral Points']*np.log( df['Spectral Points'])
+
+# Getting x and y
+x_voigt = df_voigt[['Complexity_voigt', 'lines_calculated']]  # for mulitple columns -> df_voigt[['Complexity_voigt','lines_calculated']]
+y_voigt = df_voigt['calculation_time']
+
+# Using OLS to find coerrelation and coefficient of multiple linear regression
+model = OLS(y_voigt, x_voigt)
+result = model.fit()
+print(result.summary())
+
+# or using LinearRegression()
+
+regressor = LinearRegression()
+regressor.fit(x_voigt, y_voigt)
+
+# display coefficients
+print(regressor.coef_)
+
+# Getting coefficient of multiple linear regression
+coeff = regressor.coef_
+
+# Updating Final complexity
+complexity_final_voigt = coeff[0]*df_voigt['Complexity_voigt'] + (coeff[1]*df_voigt['lines_calculated']) # + coeff[1]*df_voigt['lines_calculated'] (if more columns)
+df_voigt['complexity_final_voigt'] = complexity_final_voigt
+
+# Scatter Plot
+df_voigt.plot.scatter('complexity_final_voigt', 'calculation_time')
+
+# Saving Updated csv file
+df_voigt.to_csv(directory + "_voigt.csv", index=False)

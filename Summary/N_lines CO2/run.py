f6f92da6c2c34ca1bf4d4854ff7221ae1f6a0336
anandxkumar <anandkumar26sep00@gmail.com>
2021-08-17 23:06:58 +0530

    N_lines CO2 vs Calculation time

---
+++ Summary/N_lines CO2/run.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 21:39:26 2021
+
+@author: erwan
+"""
+
+from os.path import dirname, join
+from xexplorer import parse_files, plot
+
+def test_plot_experiment_results_sample():
+    """Sample code to explore experiment data"""
+
+    # %% DATA
+    # files (and special reading commands)
+    files = {join(dirname(__file__),"CO2 Nlines (wL and wG) vs Calculation Time.csv"):{},
+              }
+    df = parse_files(files)
+
+    print(files)
+    #%% PLOT
+
+    plot(df, xval='lines_calculated', yval='calculation_time', title="(Summary) CO2 N_lines vs Calculation Time",
+        color_mapper='optimization', size='broadening_max_width', color_mapper_scale=False)
+
+
+    return
+
+
+if __name__ == '__main__':
+    test_plot_experiment_results_sample()

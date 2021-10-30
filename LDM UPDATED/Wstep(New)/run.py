6a59265ae5e426e6145cff0fbbb257d51d7ad4e8
anandxkumar <anandkumar26sep00@gmail.com>
2021-10-31 01:43:35 +0530

    Added New Sparse Calculation benchmark

---
+++ LDM UPDATED/Wstep(New)/run.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 21:39:26 2021
+
+@author: erwan
+"""
+
+from os.path import dirname, join
+from xexplorer import parse_files, plot
+
+def test_plot_experiment_results_sample():
+    """Sample code to explore experiment data"""
+
+    # %% DATA
+    # files (and special reading commands)
+    files = {join(dirname(__file__),"Test_data.csv"):{},
+              }
+    df = parse_files(files)
+    df.drop(columns=["profiler", "dbpath", "last_modified"], inplace=True)
+
+    print(files)
+    #%% PLOT
+
+    plot(df, xval='spectral_points', yval='calculation_time', title="Spectral points(Wstep) vs Calculation Time",
+        color_mapper='name', size='truncation', color_mapper_scale=False)
+
+
+    return
+
+
+if __name__ == '__main__':
+    test_plot_experiment_results_sample()
